e11a13501a7dd7b5b3de009c461113e15f6a7161
redust <as_yhc@163.com>
2023-04-15 22:17:28 +0800

    先排序，双指针法，再去重，注意去重方法

---
+++ 哈希表/15三数之和.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+// 先排序，双指针法，再去重
+vector<vector<int>> threesum(vector<int> &nums)
+{
+    vector<vector<int>> result;
+    sort(nums.begin(),nums.end());
+    for(int i = 0;i < nums.size();i++)
+    {
+        if(nums[i] > 0 ) return result;
+        int left = i + 1;
+        int right = nums.size()-1;
+        if(i > 0 && nums[i] == nums[i-1]) continue; // i>0 没写报错
+        while(left < right)
+        {
+            if(nums[i] + nums[left] + nums[right] > 0) right--;
+            else if(nums[i] + nums[left] + nums[right] < 0) left++;
+            else
+            {
+                result.push_back(vector<int>{nums[i],nums[left],nums[right]});
+                while(left < right && nums[right] == nums[right - 1]) right--;
+                while(left < right && nums[left] == nums[left + 1]) left++;
+                left++;
+                right--;
+            }
+        }
+    }
+    return result;
+}
